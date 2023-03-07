6a27b477caa5f944e2a6dac7632b59b4adbf6672
Alireza <numerical.relativity@gmail.com>
2023-03-07 12:15:02 -0500

    Added a script to create consistent build and detect for Cactus.

---
+++ Misc/ETK/create_external_lib_for_ETK.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+## Creating an external library (thorn) for ETK (Cactus)
+## Usage: (requires net access and git tool)
+## $./me /path/to/cactus/dir
+
+if [[ $# -lt 1 ]]
+then
+	echo 'See the header of this script for the usage.'
+	exit -1
+fi
+
+## conventions:
+lib_version='1.0'
+curdir=$(pwd)
+cactus_root=$(realpath $1)
+cactus_repo="${cactus_root}/repos"
+cactus_arr="${cactus_root}/arrangements/ExternalLibraries"
+lib_name='Elliptica_ID_Reader'
+thorn="ExternalLibraries-Elliptica_ID_Reader"
+thorn_path="${cactus_repo}/${thorn}"
+git_repo="git@github.com:rashti-alireza/${lib_name}.git"
+tarball_name="${lib_name}_${lib_version}"
+
+## cleanup
+cd ${cactus_repo}
+rm -rf ${thorn}
+
+## cp ExternalLibraries-Elliptica_ID_Reader from here to cactus
+cd ${cactus_repo}
+cp -r ${curdir}/${thorn} ${thorn}
+
+## cloning
+cd ${thorn_path}/dist
+git clone --depth=1 ${git_repo}
+cd ${thorn_path}/dist/${lib_name}
+rm -rvf .git
+cd ${thorn_path}/dist
+tar -zvcf ${tarball_name}.tar.gz ${lib_name}
+rm -rvf ${lib_name}
+
+## set vars
+cd ${thorn_path}/src
+
+sed -i -E "s/^NAME=.+/NAME=${tarball_name}/g" build.sh
+
+## set arrangments(these are compiled by cactus)
+cd ${cactus_arr}
+rm -rf ${lib_name}
+ln -s  ../../repos/ExternalLibraries-${lib_name} ${lib_name}
+
+
+
+
+
+
+
